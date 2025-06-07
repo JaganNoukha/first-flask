203bf9c53f3afae1c5f94c72a271f56c158b7339
Jagan-dev <jagan@Noukha.in>
2025-06-07 23:37:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/noukha')
 def hello_world():
-    return "Hello, World! This is a basic Flask app by Jagan."
+    return "Hello, World! This is a basic Flask."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
