761d4b959df4e3dfdf625e7fa54ff6aed5b47073
Jagan-dev <jagan@Noukha.in>
2025-06-08 12:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/noukha')
-def hello_world():
-    return "Hello, World! This is a basic Flask by Jagan."
+@app.route('/') # This defines the route for the root URL
+def home_page():
+    return "Hello from my Flask app!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# ... other routes you might have ...
