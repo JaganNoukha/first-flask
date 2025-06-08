59a0e5a5f15c413a2372ca50e2d63255c565f70f
Jagan-dev <jagan@Noukha.in>
2025-06-08 13:04:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/') # This defines the route for the root URL
 def home_page():
-    return "Hello from my Flask app!"
+    return "Hello Jagan"
 
 # ... other routes you might have ...
